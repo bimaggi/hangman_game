015cdf6e07e2917b36120b8b39071a9dd64ea40b
bimaggi <bmg.olivier@gmail.com>
2021-11-08 11:00:38 -0300

    firts commit

---
+++ src/components/HangmanForm.vue
@@ -0,0 +1,74 @@
+<template>
+<div>
+  <div
+    v-if="stageInitial === 'word'"
+  >
+    <label for="">
+      {{steps.setWord.title}}
+    </label>
+    <input
+      type="text"
+      v-model="word"
+    >
+    <button
+     @click="setWord"
+    >
+      {{steps.setWord.button}}
+    </button>
+  </div>
+  <div
+    v-if="stageInitial === 'tip'"
+  >
+    <label
+      for="">
+      {{steps.setTip.title}}
+    </label>
+    <input
+      type="text"
+      v-model="tip"
+    >
+    <button
+      @click="setTip"
+    >
+      {{steps.setTip.button}}
+    </button>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HangmanForm',
+  data() {
+    return {
+      stageInitial: 'word',
+      tip: null,
+      word: null,
+      actualWord: null,
+      actuakTip: null,
+      steps: {
+        setWord: {
+          title: 'Defina a palavra',
+          button: 'Próximo',
+        },
+        setTip: {
+          title: 'Defina a dica',
+          button: 'Jogar',
+        },
+      },
+    }
+  },
+  methods: {
+    setWord() {
+      this.actualWord = this.word
+      this.$emit('setWord', this.actualWord)
+      this.stageInitial = 'tip'
+    },
+    setTip() {
+      this.actualTip = this.tip
+      this.$emit('setTip', this.actualTip)
+    },
+
+  },
+}
+</script>
--- src/components/HangmanGame.vue
+++ src/components/HangmanGame.vue
@@ -1,14 +1,57 @@
 <template>
-   <app-header/>
+  <div class="hangman">
+    <app-header/>
+    <section
+      v-if=" stage === 'init'"
+    >
+      <hangman-form
+        @setWord="setWord"
+        @setTip="setTip"
+      />
+    </section>
+    <section
+      v-if=" stage === 'play'"
+    >
+      <hangman-play
+        :isWord="isWord"
+        :isTip="isTip"
+        :error="error"
+        :validateLetter="validateLetter"
+        :letters="letters"
+      />
+    </section>
+  </div>
 </template>
-
 <script>
 import AppHeader from '@/components/AppHeader'
+import HangmanForm from '@/components/HangmanForm'
 
 export default {
   name: 'HangmanGame',
   components: {
     AppHeader,
+    HangmanForm,
+  },
+  data() {
+    return {
+      stage: 'init',
+      isWord: null,
+      isTip: null,
+      error: 0,
+      letters: [],
+    }
+  },
+  methods: {
+    setWord(value) {
+      this.isWord = value
+    },
+    setTip(value) {
+      this.isTip = value
+      this.stage = 'play'
+    },
+    validateLetter(value) {
+      return value === 'a'
+    },
   },
 }
 </script>
